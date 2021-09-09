cff2014038dbb165afb85295c692e32d2d412070
AlexandraSFR <alexandrafloresreynoso@gmail.com>
2021-09-09 01:13:20 -0500

    VALIDACIONES CON ESTILO
    ya les puse el estilo con jquery

--- PROYECTO_NEATBEANS/src/main/webapp/validar_login.js
+++ PROYECTO_NEATBEANS/src/main/webapp/validar_login.js
@@ -1,7 +1,4 @@
-
-
-
-function validar(){
+    function validar(){
     var usuario,contraseña,expresion;
     usuario=document.getElementById("usuario_login").value;
     contraseña=document.getElementById("contraseña_login").value;
@@ -9,23 +6,23 @@ function validar(){
     
    
     if(usuario ==="" || contraseña===""){
-        alert("Faltan campos para ingresar");
+        //alert("Faltan campos para ingresar");
         return false;
     }
     else if(usuario.length>21 || usuario.length<4){
-         alert("El usuario debe de tener minimo 4 caracteres y maximo 20");
+         //alert("El usuario debe de tener minimo 4 caracteres y maximo 20");
         return false;
     }
     else if(contraseña.length !== 8){
-         alert("La contraseña debe de tener 8 caracteres");
+         //alert("La contraseña debe de tener 8 caracteres");
         return false;
     }
     else if(!expresion.test(usuario)){
-        alert("Valores no validos");
+        //alert("Valores no validos");
         return false;
     }
     else if(!expresion.test(contraseña)){
-        alert("Valores no validos");
+        //alert("Valores no validos");
         return false;
     }
    
